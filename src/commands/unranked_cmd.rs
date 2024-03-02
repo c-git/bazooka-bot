7f0b1e8cefbe5eae8fbf65bea06718e631f445e1
One <43485962+c-git@users.noreply.github.com>
2024-03-01 22:45:42 -0500

    feat: add auth to reset command

--- src/commands/unranked_cmd.rs
+++ src/commands/unranked_cmd.rs
@@ -4,7 +4,7 @@ use tracing::instrument;
 
 use self::{idea::idea, score::score};
 use crate::{
-    commands::{call_to_parent_command, tracing_handler_start},
+    commands::{call_to_parent_command, is_auth, tracing_handler_start},
     Context,
 };
 
@@ -17,12 +17,7 @@ mod score;
     track_edits,
     aliases("ur"),
     subcommand_required,
-    subcommands(
-        "idea", 
-        "score", 
-        // TODO 2: Enable functions
-        // "schedule_reset"
-    )
+    subcommands("idea", "score", "schedule_reset")
 )]
 #[instrument(name = "unranked", skip(ctx))]
 /// Commands related to the Unranked Challenge [aliases("ur")]
@@ -30,10 +25,15 @@ pub async fn unranked(ctx: Context<'_>) -> anyhow::Result<()> {
     call_to_parent_command(ctx).await
 }
 
-#[poise::command(hide_in_help, prefix_command, slash_command, track_edits)]
+#[poise::command(
+    hide_in_help,
+    prefix_command,
+    slash_command,
+    track_edits,
+    check = "is_auth"
+)]
 #[instrument(name = "schedule_reset", skip(ctx))]
 pub async fn schedule_reset(ctx: Context<'_>) -> anyhow::Result<()> {
-    // TODO 2: Require Auth
     tracing_handler_start(&ctx).await;
     todo!()
 }
